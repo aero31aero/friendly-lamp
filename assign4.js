b39c8d4def7f82df2b4bc303e558fbe71999b6b9
VishwangiShah <vishroxs1998@gmail.com>
2017-06-26 12:30:12 +0530

    Third task done

--- assign4.js
+++ assign4.js
@@ -29,3 +29,31 @@ timedisplay.innerHTML="Hurray!"; //display finishing message
 }, 1000);
 };
 };
+task3 = function() {
+var getColorString = function(color) {
+return "rgb(" + color.red + "," + color.green + "," + color.blue + ")";
+}
+var getColor = function(red, green, blue){
+return {
+red: red,
+green: green,
+blue: blue,
+};
+}
+var getColorInBetween = function(color1, color2,percent){
+var getMiddleNumber = function(num1, num2, percent){
+var ratio = percent/100;
+return Math.floor(num1*ratio + num2*(1-ratio));
+}
+var red = getMiddleNumber(color1.red, color2.red, percent);
+var green = getMiddleNumber(color1.green, color2.green, percent);
+var blue = getMiddleNumber(color1.blue, color2.blue, percent);
+return getColor(red, green, blue);
+}
+
+var color1 = getColor(250,100,200);
+var color2 = getColor(200,250,250);
+var color3 = getColorInBetween(color1,color2,25);
+console.log(color3); // See the output in JavaScript Console.
+
+};
