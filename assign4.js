c2fda730b4a37bb5c6cdce808e02dfdd84f79fbb
VishwangiShah <vishroxs1998@gmail.com>
2017-06-26 12:01:39 +0530

    Task 2 done!

--- assign4.js
+++ assign4.js
@@ -8,3 +8,24 @@ return colorstring;}
 var body = document.getElementsByTagName('body')[0];
 body.style.background = getRandomColor();
 };
+var task2 = function() {
+var body = document.getElementsByTagName('body')[0];
+body.addEventListener("keyup", function(keyevent) {
+    if (keyevent.keyCode == 13) { // enter key keycode
+        startTimer(5);
+        return false;
+    }
+});
+var timedisplay = document.getElementById('timedisplay');
+var startTimer = function(seconds){
+var interval = setInterval(function(){
+seconds--;
+                        task1();
+                        timedisplay.innerhtml=seconds;
+if (seconds < 0){
+clearInterval(interval); // delete the timer
+timedisplay.innerHTML="Hurray!"; //display finishing message
+}
+}, 1000);
+};
+};
